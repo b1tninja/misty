69df237c797c4d34e731c5afa68faf78e4fde335
b1tninja <4584655+b1tninja@users.noreply.github.com>
2021-02-19 20:21:53 -0800

    Relocated data dirs

--- misty/config.py
+++ misty/config.py
@@ -1,7 +1,6 @@
 import os
 
-
 CORPUS_BASEDIR = os.path.join(os.path.dirname(__file__), 'corpus')
-TTS_BASEDIR = os.path.join(CORPUS_BASEDIR, 'tts')
-WHOOSH_INDEX_BASEDIR = os.path.join(CORPUS_BASEDIR, 'whoosh')
+TTS_BASEDIR = os.path.join(os.getcwd(), 'tts')
+WHOOSH_INDEX_BASEDIR = os.path.join(os.getcwd(), 'whoosh')
 
