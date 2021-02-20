f62fbd7cb3f571e588f42e4e0371ed9cc9650d7d
b1tninja <4584655+b1tninja@users.noreply.github.com>
2021-02-19 20:22:55 -0800

    mkdir() uses debug log instead of speaking

--- misty/utils.py
+++ misty/utils.py
@@ -5,7 +5,7 @@ from itertools import cycle
 import bcolors
 
 
-from . import voices, tts, voice_desc
+from . import voices, tts, voice_desc, logger
 
 voice_iter = cycle(voices.keys())
 
@@ -17,7 +17,7 @@ def current_speaker():
     voice_id = tts.getProperty('voice')
     return voice_desc.get(voice_id).get('name')
 
-def print_and_say(text, print_prefix=None, print_suffix=None, next_voice=True):
+def print_and_say(text, print_prefix=None, print_suffix=None, next_voice=False):
     print(f"{bcolors.ITALIC}{current_speaker():>12}{bcolors.END}:\t{print_prefix or ''}{text}{print_suffix or ''}")
     tts.say(text)
 
@@ -30,7 +30,7 @@ def print_and_say(text, print_prefix=None, print_suffix=None, next_voice=True):
 def mkdir(path):
     if not os.path.exists(path):
         os.mkdir(path)
-        print_and_say(f"Creating {os.path.basename(path)} folder.")
+        logger.info(f"Creating {os.path.basename(path)} folder.")
         return True
     else:
         assert os.path.isdir(path)
