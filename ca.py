0580b5ab049edfa815eb5cce23703cdc81a39095
b1tninja <4584655+b1tninja@users.noreply.github.com>
2021-02-26 06:25:11 -0800

    "Just follow these simple rules"

--- ca.py
+++ ca.py
@@ -65,13 +65,8 @@ def get_dats_and_lobs(path, prefixes=None, txt=True):
     return dats, lobs
 
 
-for basename in os.listdir(LEGINFO_BASEDIR):
-    if not basename.endswith('.zip'):
-        continue
-
-    path = os.path.join(LEGINFO_BASEDIR, basename)
-    if not os.path.isfile(path):
-        continue
+def get_pubinfo(path):
+    assert os.path.isfile(path)
 
     json_path = path + '.json'
 
@@ -79,8 +74,6 @@ for basename in os.listdir(LEGINFO_BASEDIR):
         assert os.path.isfile(json_path)
         with closing(open(json_path, 'r')) as fh:
             pubinfo = json.load(fh)
-            dats = pubinfo['dats']
-            lobs = pubinfo['lobs']
     else:
         dats, lobs = get_dats_and_lobs(path, prefixes=['LAW', 'CODE'])
         logging.info(f'Writting json version of dats and lobs found "{path}" to "{os.path.basename(json_path)}".')
@@ -88,31 +81,53 @@ for basename in os.listdir(LEGINFO_BASEDIR):
             pubinfo = dict(dats=dats, lobs=lobs)
             json.dump(pubinfo, fh)
 
-    # codes_tbl = dict([(CODE, TITLE) for (CODE, TITLE) in dats['CODES_TBL.dat']])
-    required = ['CODES_TBL', 'LAW_SECTION_TBL', 'LAW_TOC_SECTIONS_TBL']
-    if any([r + '.dat' not in dats for r in required]):
-        logging.warn(f"Skipping {path}... missing required sections.")
-        continue
+    return pubinfo
 
-    codes_tbl = dict(dats['CODES_TBL.dat'])
 
-    for row in dats.get('LAW_TOC_TBL.dat', []):
-        LAW_CODE, DIVISION, TITLE, PART, CHAPTER, ARTICLE, HEADING, ACTIVE_FLG, TRANS_UID, TRANS_UPDATE, NODE_SEQUENCE, NODE_LEVEL, NODE_POSITION, NODE_TREEPATH, CONTAINS_LAW_SECTIONS, HISTORY_NOTE, OP_STATUES, OP_CHAPTER, OP_SECTION = row
-        print(row)
+def get_pubinfos(basedir):
+    for basename in os.listdir(basedir):
+        if not basename.endswith('.zip'):
+            continue
 
-    for row in dats.get('LAW_TOC_SECTIONS_TBL.dat', []):
-        ID, LAW_CODE, NODE_TREEPATH, SECTION_NUM, SECTION_ORDER, TITLE, OP_STATUES, OP_CHAPTER, OP_SECTION, TRANS_UID, TRANS_UPDATE, LAW_SECTION_VERSION_ID, SEQ_NUM = row
-        print(TITLE)
+        path = os.path.join(basedir, basename)
 
-    codes = dict()
-    for row in dats.get('LAW_SECTION_TBL.dat', []):
-        ID, LAW_CODE, SECTION_NUM, OP_STATUES, OP_CHAPTER, OP_SECTION, EFFECTIVE_DATE, LAW_SECTION_VERSION_ID, DIVISION, TITLE, PART, CHAPTER, ARTICLE, HISTORY, LOB_FILE, ACTIVE_FLG, TRANS_UID, TRANS_UPDATE = row
-        if EFFECTIVE_DATE is not None:
-            EFFECTIVE_DATE = datetime.datetime.fromisoformat(EFFECTIVE_DATE).date()
+        if not os.path.isfile(path):
+            continue
 
-        TEXT = lobs.get(LOB_FILE)
+        pubinfo = get_pubinfo(path)
+        yield path, pubinfo
 
-        codes.setdefault(LAW_CODE, dict())
-        codes[LAW_CODE].setdefault(OP_SECTION, dict())
-        codes[LAW_CODE][OP_SECTION][ID] = TEXT
 
+if __name__ == '__main__':
+    for path, pubinfo in get_pubinfos(LEGINFO_BASEDIR):
+
+        dats = pubinfo['dats']
+        lobs = pubinfo['lobs']
+
+        required = ['CODES_TBL', 'LAW_SECTION_TBL', 'LAW_TOC_SECTIONS_TBL']
+        if any([r + '.dat' not in dats for r in required]):
+            logging.warn(f"Skipping {path}... missing required sections.")
+            continue
+
+        codes_tbl = dict(dats['CODES_TBL.dat'])
+
+        for row in dats.get('LAW_TOC_TBL.dat', []):
+            LAW_CODE, DIVISION, TITLE, PART, CHAPTER, ARTICLE, HEADING, ACTIVE_FLG, TRANS_UID, TRANS_UPDATE, NODE_SEQUENCE, NODE_LEVEL, NODE_POSITION, NODE_TREEPATH, CONTAINS_LAW_SECTIONS, HISTORY_NOTE, OP_STATUES, OP_CHAPTER, OP_SECTION = row
+            print(HEADING)
+
+        for row in dats.get('LAW_TOC_SECTIONS_TBL.dat', []):
+            ID, LAW_CODE, NODE_TREEPATH, SECTION_NUM, SECTION_ORDER, TITLE, OP_STATUES, OP_CHAPTER, OP_SECTION, TRANS_UID, TRANS_UPDATE, LAW_SECTION_VERSION_ID, SEQ_NUM = row
+            print(LAW_CODE, NODE_TREEPATH, SECTION_NUM, SECTION_ORDER)
+
+        codes = dict()
+        for row in dats.get('LAW_SECTION_TBL.dat', []):
+            ID, LAW_CODE, SECTION_NUM, OP_STATUES, OP_CHAPTER, OP_SECTION, EFFECTIVE_DATE, LAW_SECTION_VERSION_ID, DIVISION, TITLE, PART, CHAPTER, ARTICLE, HISTORY, LOB_FILE, ACTIVE_FLG, TRANS_UID, TRANS_UPDATE = row
+            if EFFECTIVE_DATE is not None:
+                EFFECTIVE_DATE = datetime.datetime.fromisoformat(EFFECTIVE_DATE).date()
+
+            TEXT = lobs.get(LOB_FILE)
+            print(TEXT)
+
+            codes.setdefault(LAW_CODE, dict())
+            codes[LAW_CODE].setdefault(OP_SECTION, dict())
+            codes[LAW_CODE][OP_SECTION][ID] = TEXT
