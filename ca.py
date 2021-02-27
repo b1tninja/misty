62841dbd006d5fa19cab563f29fa54f15f694f37
b1tninja <4584655+b1tninja@users.noreply.github.com>
2021-02-26 17:28:59 -0800

    parse some fields such as NodeTree, datetimes, etc. bcolors... and the rest is history

--- ca.py
+++ ca.py
@@ -6,11 +6,13 @@ import os
 import zipfile
 from contextlib import closing
 
+import bcolors
 import html2text
 import tqdm
 
 LEGINFO_BASEDIR = r"D:\downloads.leginfo.legislature.ca.gov"
 ENCODING = 'utf-8'
+CURRENT_YEAR = datetime.datetime.today().year
 
 
 # wget --mirror https://downloads.leginfo.legislature.ca.gov/
@@ -130,12 +132,43 @@ def starg(func):
 def LawTocTblDict(LAW_CODE, DIVISION, TITLE, PART, CHAPTER, ARTICLE, HEADING, ACTIVE_FLG, TRANS_UID, TRANS_UPDATE,
                   NODE_SEQUENCE, NODE_LEVEL, NODE_POSITION, NODE_TREEPATH, CONTAINS_LAW_SECTIONS, HISTORY_NOTE,
                   OP_STATUES, OP_CHAPTER, OP_SECTION):
-    return dict(**locals())
+    # if DIVISION:
+    #     DIVISION = float(DIVISION.rstrip('.'))
+    # if TITLE:
+    #     TITLE = int(float(TITLE.rstrip('.')))
+    # if PART:
+    #     PART = int(float(PART.rstrip('.')))
+    # if CHAPTER:
+    #     CHAPTER = int(float(CHAPTER.rstrip('.')))
+    # if ARTICLE:
+    #     ARTICLE = int(float(ARTICLE.rstrip('.')))
+    # HEADING
+    ACTIVE_FLG = 'Y' == ACTIVE_FLG
+    # TRANS_UID
+    TRANS_UPDATE = datetime.datetime.fromisoformat(TRANS_UPDATE)
+    NODE_SEQUENCE = int(NODE_SEQUENCE)
+    NODE_LEVEL = int(NODE_LEVEL)
+    NODE_POSITION = int(NODE_POSITION)
+    NODE_TREEPATH = tuple(map(int, NODE_TREEPATH.split('.')))
+    CONTAINS_LAW_SECTIONS = 'Y' == CONTAINS_LAW_SECTIONS
+    # HISTORY_NOTE
+    # OP_STATUES = float(OP_STATUES or 0)
+    # OP_CHAPTER = float(OP_CHAPTER or 0)
+    # OP_SECTION = float(OP_SECTION or 0)
+
+    d = dict(**locals())
+
+    # pprint.pprint(d)
+    # assert 1849 <= OP_STATUES <= CURRENT_YEAR
+
+    return d
 
 
 @starg
 def LawTocSectionsTblDict(ID, LAW_CODE, NODE_TREEPATH, SECTION_NUM, SECTION_ORDER, TITLE, OP_STATUES, OP_CHAPTER,
                           OP_SECTION, TRANS_UID, TRANS_UPDATE, LAW_SECTION_VERSION_ID, SEQ_NUM):
+    NODE_TREEPATH = tuple(map(int, NODE_TREEPATH.split('.')))
+    TRANS_UPDATE = datetime.datetime.fromisoformat(TRANS_UPDATE)
     return dict(**locals())
 
 
@@ -150,13 +183,21 @@ def LawSectionTblDict(ID, LAW_CODE, SECTION_NUM, OP_STATUES, OP_CHAPTER, OP_SECT
 
 @dxt
 def parse_datlobs(LOBS, *, CODES_TBL, LAW_TOC_TBL, LAW_SECTION_TBL, LAW_TOC_SECTIONS_TBL):
-    # CODES_TBL = OrderedDict(CODES_TBL)
-    #
-    # for d in map(LawTocTblDict, LAW_TOC_TBL):
-    #     print(d)
-    #
-    # for d in map(LawTocSectionsTblDict, LAW_TOC_SECTIONS_TBL):
-    #     print(d)
+    CODES_TBL = dict(CODES_TBL)
+
+    # print(next(map(LawTocTblDict, LAW_TOC_TBL)))
+    toc_tbl = dict()
+    for d in map(LawTocTblDict, LAW_TOC_TBL):
+        toc_tbl.setdefault(d['LAW_CODE'],
+                           {}).setdefault(d['DIVISION'],
+                                          {}).setdefault(d['CHAPTER'],
+                                                         {}).__setitem__(d['ARTICLE'], d)
+
+    # toc_tbl[LAW_CODE][DIVISION][CHAPTER][ARTICLE]
+    code_section_titles = dict()
+    for d in map(LawTocSectionsTblDict, LAW_TOC_SECTIONS_TBL):
+        code_section_titles.setdefault(d['LAW_CODE'],
+                                       {}).__setitem__(d['SECTION_NUM'], d)
 
     codes = dict()
     for ID, LAW_CODE, SECTION_NUM, OP_STATUES, OP_CHAPTER, OP_SECTION, EFFECTIVE_DATE, LAW_SECTION_VERSION_ID, DIVISION, TITLE, PART, CHAPTER, ARTICLE, HISTORY, LOB_FILE, ACTIVE_FLG, TRANS_UID, TRANS_UPDATE in LAW_SECTION_TBL:
@@ -165,14 +206,23 @@ def parse_datlobs(LOBS, *, CODES_TBL, LAW_TOC_TBL, LAW_SECTION_TBL, LAW_TOC_SECT
         codes[LAW_CODE].setdefault(OP_SECTION, dict())
         codes[LAW_CODE][OP_SECTION][ID] = LOB
 
+        # o = io.StringIO()
+        # o.writelines([])
+        _toc = toc_tbl[LAW_CODE][DIVISION][CHAPTER][ARTICLE]
+        print(f"{bcolors.BLUE}{CODES_TBL[LAW_CODE]}{bcolors.ENDC}")
+        print(
+            f"    {bcolors.UNDERLINE}{_toc['HEADING']}{bcolors.ENDC} ({bcolors.ITALIC}{_toc['HISTORY_NOTE']}{bcolors.ENDC})")
+        print(f"        SOMETHING AND THE REST IS ({HISTORY})")
+        print(f"{bcolors.BOLD}{code_section_titles[LAW_CODE][SECTION_NUM]['TITLE'] or ''}{bcolors.ENDC}")
+        print(LOB)
+
     return codes
 
 
 if __name__ == '__main__':
 
     for path, (dats, lobs) in get_datses_and_lobses(LEGINFO_BASEDIR, prefixes=['LAW', 'CODE']):
-        try:
-            codes = parse_datlobs(lobs, **dats)
-            print(codes)
-        except TypeError as e:
-            logging.warning(f"Skipping {path}... {e}.")
+        # try:
+        codes = parse_datlobs(lobs, **dats)
+    # except TypeError as e:
+    #     logging.warning(f"Skipping {path}... {e}.")
