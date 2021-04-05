7cc36fbe4d3fa3b48e2c90ef2418e705f924a9ee
Justin Capella <justincapella@gmail.com>
2021-04-05 14:43:06 -0700

    BUGFIX: sorting was based on key, which was previously PrimaryDocNumber

--- sacrec_tsv.py
+++ sacrec_tsv.py
@@ -6,6 +6,10 @@ from operator import itemgetter
 
 from tqdm import tqdm
 
+def d2c(path, d):
+    pass
+
+
 if __name__ == '__main__':
     root_dir = 'sac'
     for name in tqdm(os.listdir(root_dir)):
@@ -27,7 +31,7 @@ if __name__ == '__main__':
         # Make uniq by PrimaryDocNumber and SecondaryDocNumber.
         # TODO: consider frozenset(o.items()) as __hash__
         ud = dict([(int(o['ID']), o) for o in rows])
-        desc_rows = [ud[key] for key in sorted(ud.keys(), reverse=True)]
+        dr = sorted(ud.values(), key=itemgetter('PrimaryDocNumber'))
 
         if not rows:
             logging.warning("No rows!")
@@ -38,12 +42,11 @@ if __name__ == '__main__':
         csv_name = name + '.csv'
         csv_path = os.path.join(root_dir, csv_name)
 
-        sorted(rows, key=itemgetter('PrimaryDocNumber'))
 
         with open(csv_path, 'w') as fh:
-            ignore = ['ID', 'SecondaryDocNumber', 'FilingCode', 'BookNumber', 'NumberOfPages', 'BookType']
+            i = ['ID', 'SecondaryDocNumber', 'FilingCode', 'BookNumber', 'NumberOfPages', 'BookType']
             reprs = {'Names': lambda v: '|'.join(v.split("<br/>"))}
 
-            dw = csv.DictWriter(fh, [k for k in keys if k not in ignore], delimiter='\t')
+            dw = csv.DictWriter(fh, [k for k in keys if k not in i], delimiter='\t')
             dw.writeheader()
-            dw.writerows([dict(((k, reprs.get(k, lambda v: v)(v)) for k, v in r.items() if k not in ignore)) for r in desc_rows])
+            dw.writerows([dict(((k, reprs.get(k, lambda v: v)(v)) for k, v in r.items() if k not in i)) for r in dr])
