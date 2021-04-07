07929af54390d99c586f5cb9f370f839901ff656
Justin Capella <justincapella@gmail.com>
2021-04-06 22:29:35 -0700

    More concurrent, tqdm

--- sacass.py
+++ sacass.py
@@ -1,3 +1,4 @@
+import tqdm
 # Address.csv
 # https://prod-hub-indexer.s3.amazonaws.com/files/54b1835ffb7b4e728a3506fe1a23618d/1/full/2226/54b1835ffb7b4e728a3506fe1a23618d_1_full_2226.csv
 
@@ -9,10 +10,9 @@ import random
 from asyncio import Semaphore
 
 import aiohttp
-import tqdm
 
 BASE_DIR = 'sacass'
-sem = Semaphore(1)
+c = 25
 
 import asyncio
 from aiohttp import ContentTypeError
@@ -26,22 +26,20 @@ async def apn_json(session: aiohttp.ClientSession, apn: str):
             o = json.load(fh)
             return apn, o
 
-    async with sem:
-
-        async with session.get(url) as response:
-            assert response.status == 200
-            try:
-                o = dict(await response.json())
-            except ContentTypeError:
-                logging.warning("ContentTypeError for %s" % url)
-            else:
-                with open(json_path, 'w') as fh:
-                    json.dump(o, fh)
-                return apn, o
+    async with session.get(url) as response:
+        assert response.status == 200
+        try:
+            o = dict(await response.json())
+        except ContentTypeError:
+            logging.warning("ContentTypeError for %s" % url)
+        else:
+            with open(json_path, 'w') as fh:
+                json.dump(o, fh)
+            return apn, o
 
 
 async def download_apns(session: aiohttp.ClientSession, apns: list):
-    download_futures = [apn_json(session, apn) for apn in tqdm.tqdm(apns)]
+    download_futures = [apn_json(session, apn) for apn in apns]
     for download_future in asyncio.as_completed(download_futures):
         try:
             ret = await download_future
@@ -52,7 +50,7 @@ async def download_apns(session: aiohttp.ClientSession, apns: list):
         else:
             if ret is not None:
                 apn, data = ret
-                print(apn, data.get('FullAddress'))
+                # print(apn, data.get('FullAddress'))
 
     return True
 
@@ -64,8 +62,8 @@ async def main():
 
     random.shuffle(apns)
     async with aiohttp.ClientSession() as session:
-        while apns:
-            await download_apns(session, [apns.pop() for n in range(10)])
+        for n in tqdm.tqdm(range(0, len(apns), c)):
+            await download_apns(session, apns[n:][:c])
 
 
 if __name__ == '__main__':
