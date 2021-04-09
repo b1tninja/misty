75a4040f0b527e18dc3f65e33b4d312b2920c5d6
Justin Capella <justincapella@gmail.com>
2021-04-09 07:29:03 -0700

    wrap imports in try/except in __init__.py

--- misty/__init__.py
+++ misty/__init__.py
@@ -1,23 +1,27 @@
 import logging
-import random
 import re
-
-import nltk
-import pyttsx3
-from nltk.corpus import stopwords
-
-nltk.download('stopwords')
-
-voice_re = re.compile(
-    r"(?P<vendor>Microsoft) (?P<name>\w+)(?:\s(?P<platform>\w+))? - (?P<lang>\w+)(?:\s*\((?P<region>[^)]+)\))?")
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-tts = pyttsx3.init()
+try:
+    import nltk
+except (ImportError, ModuleNotFoundError):
+    logging.warning("nltk library provides stop words and categories for words")
+else:
+    from nltk.corpus import stopwords
 
-voices = tts.getProperty('voices')
-random.shuffle(voices)
-voices = dict([(voice.id, voice) for voice in voices if 'english' in voice.name.lower()])
-voice_desc = dict([(voice.id, voice_re.match(voice.name).groupdict()) for voice in voices.values()])
-en_stopwords = stopwords.words('english')
+    nltk.download('stopwords')
+    en_stopwords = stopwords.words('english')
+
+try:
+    import pyttsx3
+except (ImportError, ModuleNotFoundError):
+    logging.warning("pyttsx3 offers Microsoft text-to-speech support")
+else:
+    tts = pyttsx3.init()
+
+    voices = tts.getProperty('voices')
+    voice_re = re.compile(r"(?P<vendor>Microsoft) (?P<name>\w+)(?:\s(?P<platform>\w+))? - (?P<lang>\w+)(?:\s*\((?P<region>[^)]+)\))?")
+    voices = dict([(voice.id, voice) for voice in voices if 'english' in voice.name.lower()])
+    voice_desc = dict([(voice.id, voice_re.match(voice.name).groupdict()) for voice in voices.values()])
